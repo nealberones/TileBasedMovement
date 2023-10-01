70d84f1c3d77de404e4556eb40bb8d1fbd33b084
Josh <andy.lorenzo@obf.ateneo.edu>
2023-10-01 19:56:59 +0800

    Missing Forced movement, Animations, Tp tile

--- player.gd
+++ player.gd
@@ -2,6 +2,7 @@ extends Sprite2D
 
 @onready var tile_map = $"../TileMap"
 @onready var sprite_2d = $Sprite2D
+@onready var ray_cast_2d = $RayCast2D
 const tile_size = 16  # Adjust this based on your tile size
 var current_tile = Vector2(1, 1)
 
@@ -41,8 +42,14 @@ func move(direction: Vector2):
 	
 	var tile_data: TileData = tile_map.get_cell_tile_data(0,target_tile)
 	
-	if tile_data.get_custom_data('walkable') == false:
+	if tile_data.get_custom_data('Walkable') == false:
 		return
+	
+	ray_cast_2d.target_position = direction * 16
+	ray_cast_2d.force_raycast_update()
+		
+	if ray_cast_2d.is_colliding():
+		return 
 	
 	#move player
 	is_moving = true
@@ -50,3 +57,53 @@ func move(direction: Vector2):
 	global_position = tile_map.map_to_local(target_tile)
 	
 	sprite_2d.global_position = tile_map.map_to_local(current_tile)
+
+	if tile_data.get_custom_data('Teleporter') == true:
+			#teleport to random tile or other teleporter (Make sure to 'paint' custom data value on tileset)
+		teleport()
+	if tile_data.get_custom_data('Force') == true:
+			#forced movement tile
+		var force_direction: Vector2 = tile_data.get_custom_data('ForcDirection')
+		move_force(force_direction)
+
+func move_force(direction: Vector2):
+	# Check if the force movement is valid (not blocked by obstacles)
+	var target_tile: Vector2i = current_tile + direction
+
+	var tile_data_force: TileData = tile_map.get_cell_tile_data(0, target_tile)
+
+	if tile_data_force.get_custom_data('Walkable') == false:
+		return
+
+	# Move the player to the force direction
+	is_moving = true
+	global_position = tile_map.map_to_local(target_tile)
+	current_tile = target_tile
+
+	# Update the sprite position
+	sprite_2d.global_position = tile_map.map_to_local(current_tile)
+
+func teleport():
+	# Find all teleporter tiles in the map
+	var teleporter_tiles: Array = []
+
+	# Iterate through all tiles to find those with the 'Teleporter' custom value
+	for x in range(tile_map.get_used_rect().size.x):
+		for y in range(tile_map.get_used_rect().size.y):
+			var tile_pos = Vector2i(x, y)
+			var tile_data_tp: TileData = tile_map.get_cell_tile_data(0,tile_pos)
+			
+			if tile_data_tp.get_custom_data('Teleporter') == true:
+				teleporter_tiles.append(tile_pos)
+
+	if teleporter_tiles.size() > 0:
+		# Pick a random teleporter tile
+		var random_index: int = randi() % teleporter_tiles.size()
+		var random_teleporter: Vector2i = teleporter_tiles[random_index]
+
+		# Set the player's position to the teleporter tile
+		global_position = tile_map.map_to_local(random_teleporter)
+
+		# Update the current tile
+		current_tile = random_teleporter
+
